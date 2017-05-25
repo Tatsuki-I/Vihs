2c82f2e6b8c7aaf60fe3c131b3143ec2d18e4678
Tatsuki <tatsuki@konokaze.jp>
2017-05-25 14:45:16 +0900

    Update delLine.hs
    0以下の時に処理を行わない
    
    Update delLine.hs
    
    複数行削除に対応

--- lib/delLine.hs
+++ lib/delLine.hs
@@ -1,9 +1,13 @@
-deleteLine :: [String] -> Int -> [String]
-deleteLine str n | n > length str = str
-                 | otherwise = (init $ take n str) ++ (reverse $ take ((length str) - n) $ reverse str)
+deleteLine :: [String] -> Int -> Int ->[String]
+deleteLine str line n | line <= length str && line > 0 && line + n - 1 <= length str = (take (line - n) str) ++ (reverse $ take ((length str) - line) $ reverse str)
+                      | otherwise = str
 
 main = do
     print str
-    print $ deleteLine str 2
+    print $ deleteLine str 0 1
+    print $ deleteLine str 1 1
+    print $ deleteLine str 2 2
+    print $ deleteLine str 3 1
+    print $ deleteLine str 4 3
     where
-        str = ["abc", "def", "ghi"]
+        str = ["abc", "def", "ghi", "jkl", "mno"]

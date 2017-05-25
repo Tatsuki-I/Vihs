26804e5e2d575e21b49bb94a0b020414f815a88d
Tatsuki <tatsuki@konokaze.jp>
2017-05-25 22:01:16 +0900

    TUIの表示テスト

--- lib/Main.hs
+++ lib/Main.hs
@@ -1,8 +1,25 @@
 import ReadWrite
 import Delete
 
+import Brick
+import Brick.Widgets.Center
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+
+ui :: String -> Widget ()
+ui txt = 
+    withBorderStyle unicode $
+    (str txt)
+
+main :: IO ()
+main = do
+    str <- readFile "test.txt"
+    simpleMain (ui str)
+
+{-
 main = do
     str <- createBuffer "test.txt"
     print str
     print $ deleteLine str 2 2
     buffToFile "test2.txt" $ deleteLine str 2 2
+-}

7204678f21aa0533fcdb793489c9e0e563db6b8c
Tatsuki <tatsuki@konokaze.jp>
2017-05-26 23:39:21 +0900

    要素数が1からになっていたのを0からに修正

--- lib/Delete.hs
+++ lib/Delete.hs
@@ -1,5 +1,4 @@
 module Delete where
-
 deleteLine :: [String] -> Int -> Int ->[String]
-deleteLine str line n | line <= length str && line > 0 && line + n - 1 <= length str = (take (line - n) str) ++ (reverse $ take ((length str) - line) $ reverse str)
+deleteLine str line n | line <= length str && line >= 0 && line + n - 1 <= length str = (take line str) ++ (reverse $ take ((length str) - line - n) $ reverse str)
                       | otherwise = str

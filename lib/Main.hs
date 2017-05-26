9a2f89e769d715e2edec91774d9015b2719c4e4e
Tatsuki <tatsuki@konokaze.jp>
2017-05-26 20:26:18 +0900

    HSCursesのテスト

--- lib/Main.hs
+++ lib/Main.hs
@@ -1,25 +1,31 @@
 import ReadWrite
 import Delete
+import qualified UI.HSCurses.Curses as Curses
+import qualified UI.HSCurses.CursesHelper as CursesH
+import Control.Exception (finally)
+import System.Posix      (usleep)
 
-import Brick
-import Brick.Widgets.Center
-import Brick.Widgets.Border
-import Brick.Widgets.Border.Style
-
-ui :: String -> Widget ()
-ui txt = 
-    withBorderStyle unicode $
-    (str txt)
-
-main :: IO ()
-main = do
-    str <- readFile "test.txt"
-    simpleMain (ui str)
+main =
+  do
+    Curses.initCurses
+    (height, width) <- Curses.scrSize
+    strbuf <- createBuffer "test.txt"
+    let str = unlines $ take (height - 1) strbuf
+    Curses.mvWAddStr Curses.stdScr 0 0 str
+    Curses.refresh
+    Curses.getCh
+    let str2 = unlines $ take (height - 1) $ tail strbuf
+    Curses.mvWAddStr Curses.stdScr 0 0 str2
+    Curses.refresh
+    Curses.getCh
+    Curses.endWin
 
 {-
 main = do
+    str <- readFile "test.txt"
     str <- createBuffer "test.txt"
     print str
     print $ deleteLine str 2 2
     buffToFile "test2.txt" $ deleteLine str 2 2
 -}
+

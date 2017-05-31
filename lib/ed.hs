bf9f289c80783786e1e5e983b35dbfed39b328c2
Tatsuki <tatsuki@konokaze.jp>
2017-06-01 02:07:18 +0900

    不要な変数の削除
    不要な変数の削除、及び関数の返り値の型の変更

--- lib/ed.hs
+++ lib/ed.hs
@@ -1,44 +1,34 @@
 import Text.Parsec
 
-ed :: IO [String]
-ed = do
-  cmd <- getLine
-  ed' cmd [] True
+ed :: IO ()
+ed =
+  getLine >>= (\x -> ed' x [] True)
     where
-      ed' :: String -> [String] -> Bool -> IO [String]
+      ed' :: String -> [String] -> Bool -> IO ()
       ed' cmd buff saved
-        | cmd == "q" && saved = return buff
+        | cmd == "q" && saved = return ()
         | cmd == "q" && not saved = do
           putStrLn "?"
           newCmd <- getLine
           if newCmd == "q"
             then ed' newCmd buff True
             else ed' newCmd buff False
-        | cmd == "a" = do
-          newBuff <- insert
-          newCmd <- getLine
-          ed' newCmd newBuff False
-        | (last cmd) == 'i' = do
-          buff2 <- insert
-          let newBuff = iCmd buff buff2 (read $ init cmd)
-          newCmd <- getLine
-          ed' newCmd newBuff False
-        | (last cmd) == 'd' = do
-          newCmd <- getLine
-          ed' newCmd (deleteLine buff $ read $ init cmd) False
+        | cmd == "a" =
+          insert >>= (\x -> getLine >>= (\y -> ed' y x False))
+        | (last cmd) == 'i' =
+          insert >>= (\x -> getLine >>= (\y -> ed' y (iCmd buff x (read $ init cmd)) False))
+        | (last cmd) == 'd' =
+          getLine >>= (\x -> ed' x (deleteLine buff $ read $ init cmd) False)
         | cmd == "l" = do
           putStr $ unlines (map (++"$") buff)
-          newCmd <- getLine
-          ed' newCmd buff saved
+          getLine >>= (\x -> ed' x buff saved)
         | (head cmd) == 'w' = do
           buffToFile ((words cmd) !! 1) buff
           print $ length (unlines buff)
-          newCmd <- getLine
-          ed' newCmd buff True
+          getLine >>= (\x -> ed' x buff True)
         | otherwise = do
           putStrLn "?"
-          newCmd <- getLine
-          ed' newCmd buff saved
+          getLine >>= (\x -> ed' x buff saved)
 
 buffToFile :: String -> [String] -> IO ()
 buffToFile path str = writeFile path $ unlines str

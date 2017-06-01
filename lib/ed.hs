5aece3a04a3f8f9573d29f678adc5d85c8bb72e7
Tatsuki <tatsuki@konokaze.jp>
2017-06-02 00:23:07 +0900

    入力中に削除をできるようにした

--- lib/ed.hs
+++ lib/ed.hs
@@ -1,40 +1,43 @@
+import System.Console.Haskeline
+import Data.Maybe
 import Text.Parsec
 
 ed :: IO ()
 ed =
-  getLine >>= (\x -> ed' x [] True)
+  (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' x [] True)
     where
       ed' :: String -> [String] -> Bool -> IO ()
       ed' cmd buff saved
         | cmd == "q" && saved = return ()
         | cmd == "q" && not saved = do
           putStrLn "?"
-          newCmd <- getLine
+          newCmd <- fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")
           if newCmd == "q"
             then ed' newCmd buff True
             else ed' newCmd buff False
         | cmd == "a" =
-          insert >>= (\x -> getLine >>= (\y -> ed' y x False))
+          insert >>= (\x -> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\y -> ed' y x False))
         | (last cmd) == 'i' =
-          insert >>= (\x -> getLine >>= (\y -> ed' y (iCmd buff x (read $ init cmd)) False))
+          insert >>= (\x -> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\y -> ed' y (iCmd buff x (read $ init cmd)) False))
         | (last cmd) == 'd' =
-          getLine >>= (\x -> ed' x (deleteLine buff $ read $ init cmd) False)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' x (deleteLine buff $ read $ init cmd) False)
         | cmd == "l" = do
           putStr $ unlines (map (++"$") buff)
-          getLine >>= (\x -> ed' x buff saved)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' x buff saved)
         | (head cmd) == 'w' = do
           buffToFile ((words cmd) !! 1) buff
           print $ length (unlines buff)
-          getLine >>= (\x -> ed' x buff True)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' x buff True)
         | otherwise = do
           putStrLn "?"
-          getLine >>= (\x -> ed' x buff saved)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' x buff saved)
 
 buffToFile :: String -> [String] -> IO ()
 buffToFile path str = writeFile path $ unlines str
 
 iCmd :: [String] -> [String] -> Int -> [String]
-iCmd buff buff2 line = (take (line - 1) buff) ++ buff2 ++ (reverse (take ((length buff) - line + 1) (reverse buff)))
+iCmd buff buff2 line =
+  (take (line - 1) buff) ++ buff2 ++ (reverse (take ((length buff) - line + 1) (reverse buff)))
 
 insert :: IO [String]
 insert = insert' [] False
@@ -49,6 +52,7 @@ insert = insert' [] False
           else insert' (buff ++ [str]) False
 
 deleteLine :: [String] -> Int ->[String]
-deleteLine str line | line <= length str && line >= 0 && line - 1 <= length str = (take (line - 1) str) ++ (reverse . take ((length str) - line) $ reverse str)
+deleteLine str line | line <= length str && line >= 0 && line - 1 <= length str =
+  (take (line - 1) str) ++ (reverse . take ((length str) - line) $ reverse str)
 
 main = ed

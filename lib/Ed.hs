ffe9fa653cf09850cfc7d590f2fa7052473214ee
Tatsuki <tatsuki@konokaze.jp>
2017-06-02 16:10:25 +0900

    カレント行を引数に追加

--- lib/Ed.hs
+++ lib/Ed.hs
@@ -5,33 +5,33 @@ import Command
 
 ed :: IO ()
 ed =
-  (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) [] True)
+  (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) [] 0 True)
     where
-      ed' :: Command -> [String] -> Bool -> IO ()
-      ed' cmd buff saved
+      ed' :: Command -> [String] -> Int -> Bool -> IO ()
+      ed' cmd buff crrLine saved
         | (cmdName cmd) == 'q' && saved = return ()
         | (cmdName cmd) == 'q' && not saved = do
           putStrLn "?"
           newCmd <- fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")
           if (cmdName (setCmd newCmd)) == 'q'
-            then ed' (setCmd newCmd) buff True
-            else ed' (setCmd newCmd) buff False
+            then ed' (setCmd newCmd) buff crrLine True
+            else ed' (setCmd newCmd) buff crrLine False
         | (cmdName cmd) == 'a' =
-          insert >>= (\x -> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\y -> ed' (setCmd y) x False))
+          insert >>= (\x -> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\y -> ed' (setCmd y) x crrLine False))
         | (cmdName cmd) == 'i' =
-          insert >>= (\x -> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\y -> ed' (setCmd y) (iCmd buff x $ fromMaybe 0 $ addr1 cmd) False))
+          insert >>= (\x -> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\y -> ed' (setCmd y) (iCmd buff x $ fromMaybe 0 $ addr1 cmd) crrLine False))
         | (cmdName cmd) == 'd' =
-          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) (deleteLine buff $ fromMaybe 0 $ addr1 cmd) False)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) (deleteLine buff $ fromMaybe 0 $ addr1 cmd) crrLine False)
         | (cmdName cmd) == 'l' = do
           putStr $ unlines (map (++"$") buff)
-          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff saved)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff crrLine saved)
         | (cmdName cmd) == 'w' = do
           if (isNothing $ param cmd)
-            then putStrLn "?" >> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff saved)
-            else (buffToFile (fromJust (param cmd)) buff) >> (print (length (unlines buff)) >> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff True))
+            then putStrLn "?" >> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff crrLine saved)
+            else (buffToFile (fromJust (param cmd)) buff) >> (print (length (unlines buff)) >> (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff crrLine True))
         | otherwise = do
           putStrLn "?"
-          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff saved)
+          (fromMaybe "" <$> (runInputT defaultSettings $ getInputLine "")) >>= (\x -> ed' (setCmd x) buff crrLine saved)
 
 buffToFile :: String -> [String] -> IO ()
 buffToFile path str = writeFile path $ unlines str

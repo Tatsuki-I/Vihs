b495f2346e9f52410bd7c1141786299ca60cbaad
Tatsuki <tatsuki@konokaze.jp>
2017-06-02 15:19:42 +0900

    構文解析の実装

--- lib/Cmd.hs
+++ lib/Cmd.hs
@@ -1,22 +1,45 @@
+import Data.Either
 import Text.Parsec
+import Text.Parsec.String
 
-{-
-cmd :: String -> IO Int
-cmd cmdstr = do
-    num <- parseTest numbers cmdstr
-    return num
--}
+data Command = Command
+  { addr1 :: Maybe Int
+  , addr2 :: Maybe Int
+  , cmd   :: Char
+  , param :: Maybe String
+  } deriving Show 
 
-numbers = many1 digit >>= \x -> return (read x :: Int)
+setCmd :: String -> Command
+setCmd str = Command
+  (if addrs == []
+    then Nothing
+    else Just (addrs !! 0))
+  (if length addrs < 2
+    then Nothing
+    else Just (addrs !! 1))
+  (last $ (words str) !! 0)
+  (if length (words str) < 2
+    then Nothing
+    else (Just $ (words str) !! 1))
+      where
+        addrs = (parseIntList parseText $ init $ (words str) !! 0)
 
-{-
-cmd = many1 letter >>= \x -> return (read x :: String)
--}
+parseInt :: Parser Int
+parseInt = do
+  value <- many1 digit
+  return (read value)
 
-commandWithCount = do
-    count <- numbers
-    command <- (many1 letter)
-    return (count, command)
+parseText :: Parser [Int]
+parseText = parseInt `sepBy1` (char ',')
+
+parseIntList :: Parser [Int] -> String -> [Int]
+parseIntList parser input
+  = case (parse parser "" input) of
+    Left err -> []
+    Right x -> x
 
 main = do
-    parseTest commandWithCount "1dd"
+  print $ setCmd "2,3d"
+  print $ setCmd "w aaa.txt"
+  print $ setCmd "1i"
+  print $ setCmd "l"

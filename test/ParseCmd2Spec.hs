b65793c885f35f358e711ceed7686daa5dc4351f
Tatsuki <tatsuki@konokaze.jp>
2017-06-06 12:26:24 +0900

    hlintの適用

--- test/ParseCmd2Spec.hs
+++ test/ParseCmd2Spec.hs
@@ -4,13 +4,11 @@ import           ParseCmd2
 import           Test.Hspec
 
 spec :: Spec
-spec = do
+spec =
         describe "setCmd" $ do
-                it "standard" $ do
+                it "standard" $
                         setCmd "w test.txt" `shouldBe` Command {addr = Nothing, cmdName = 'w', param = Just "test.txt"}
-        describe "setCmd" $ do
-                it "standard" $ do
+                it "standard" $
                         setCmd "1,2a" `shouldBe` Command {addr = Just (AddrPair (AddrLine 1) (AddrLine 2)), cmdName = 'a', param = Nothing}
-        describe "setCmd" $ do
-                it "standard" $ do
+                it "standard" $
                         setCmd ",d" `shouldBe` Command {addr = Just (AddrPair (AddrLine 1) AddrEOF), cmdName = 'd', param = Nothing}

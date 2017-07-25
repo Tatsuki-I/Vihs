58784d202138c0e52e6b2a4fa3249fdb2e6e9666
Tatsuki <tatsuki@konokaze.jp>
2017-07-25 17:19:52 +0900

    fix indents

--- src/ParseCmd.hs
+++ src/ParseCmd.hs
@@ -4,72 +4,67 @@ import           Control.Applicative
 import qualified Text.Parsec         as P
 import           Text.Parsec.String
 
-data Command = Command
-        { addr    :: Maybe Addr
-        , cmdName :: Char
-        , param   :: Maybe String
-        } deriving (Show, Eq)
+data Command = Command { addr    :: Maybe Addr
+                       , cmdName :: Char
+                       , param   :: Maybe String
+                       } deriving (Show, Eq)
 
-data Addr
-        = AddrSingle AddrVal
-        | AddrPair  AddrVal AddrVal
-        deriving (Show, Eq)
+data Addr = AddrSingle AddrVal
+          | AddrPair  AddrVal AddrVal
+          deriving (Show, Eq)
 
-data AddrVal
-        = AddrLine Int
-        | AddrCrr Int
-        | AddrEOF
-        deriving (Show, Eq)
+data AddrVal = AddrLine Int
+             | AddrCrr Int
+             | AddrEOF
+             deriving (Show, Eq)
 
 addrCrrLine :: AddrVal
-addrCrrLine = AddrCrr 0
+addrCrrLine =  AddrCrr 0
 
-setCmd :: String -> Command
-setCmd str = case P.parse parseCmd "" str of
-        Right cmd -> cmd
-        Left  err -> Command Nothing ' ' Nothing
+setCmd     :: String -> Command
+setCmd str =  case P.parse parseCmd "" str of
+                Right cmd -> cmd
+                Left  err -> Command Nothing ' ' Nothing
                 --putStrLn ("No match: " ++ show err)
 
 parseCmd :: Parser Command
-parseCmd = Command
-        <$> parseAddr
-        <*> parseCmdName
-        <*> parseParam
+parseCmd =  Command <$> parseAddr
+                    <*> parseCmdName
+                    <*> parseParam
 
 parseAddr :: Parser (Maybe Addr)
-parseAddr = optional parseAddr'
-    where
-        parseAddr'
-                =   parseHeadToEOF
-                <|> parseCrrToEOF
-                <|> parseAddrs
+parseAddr =  optional parseAddr'
+  where
+    parseAddr' = parseHeadToEOF
+             <|> parseCrrToEOF 
+             <|> parseAddrs
 
-        parseHeadToEOF = P.char ','
-                *> pure (AddrPair (AddrLine 1) AddrEOF)
+    parseHeadToEOF = P.char ','
+                  *> pure (AddrPair (AddrLine 1) AddrEOF)
 
-        parseCrrToEOF = P.char ';'
-                *> pure (AddrPair addrCrrLine AddrEOF)
+    parseCrrToEOF = P.char ';'
+                 *> pure (AddrPair addrCrrLine AddrEOF)
 
-        parseAddrs = do
-                (x:xs) <- parseIntList
-                return $ if null xs
-                             then AddrSingle $ AddrLine x
-                             else AddrPair (AddrLine x) (AddrLine $ last xs)
+    parseAddrs = do
+                   (x:xs) <- parseIntList
+                   return $ if null xs
+                              then AddrSingle $ AddrLine x
+                              else AddrPair (AddrLine x) (AddrLine $ last xs)
 
 parseCmdName :: Parser Char
-parseCmdName = P.letter
+parseCmdName =  P.letter
 
 parseParam :: Parser (Maybe String)
-parseParam = P.spaces
-        *> (listToMaybeList <$> P.many P.anyChar)
+parseParam =  P.spaces
+          *> (listToMaybeList <$> P.many P.anyChar)
             where
-                listToMaybeList [] = Nothing
-                listToMaybeList xs = Just xs
+              listToMaybeList [] = Nothing
+              listToMaybeList xs = Just xs
 
 parseIntList :: Parser [Int]
-parseIntList = parseInt `P.sepBy1` P.char ','
+parseIntList =  parseInt `P.sepBy1` P.char ','
 
 parseInt :: Parser Int
-parseInt = do
-        value <- P.many1 P.digit
-        return (read value)
+parseInt =  do
+              value <- P.many1 P.digit
+              return (read value)
